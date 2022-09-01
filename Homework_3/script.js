51243175466b3e734fb5a96e79751471e85e0675
Sergiy Piven <seregapiven@gmail.com>
2022-09-01 20:13:36 +0300

    Completed Homework_3

---
+++ Homework_3/script.js
@@ -0,0 +1,28 @@
+let operand;
+do {
+  operand = prompt('Введите число: ');
+} while (isNumberInvalid(operand));
+  operand = Number(operand);
+function isNumberInvalid(val) {
+  return val === null || val <= 0 || val.trim() === '' || isNaN(val);
+}
+function sumOddNumbers(number) {
+  let sum = 0;
+  for (let i = 1; i <= number; i+=2) {
+    sum += i;
+  }
+  return sum;
+}
+function sumEvenNumbers(number) {
+  let sum = 0;
+  for (let i = 2; i <= number; i+=2) {
+    sum += i;
+  }
+  return sum;
+}
+alert('Сумма четных чисел: ' + sumEvenNumbers(operand));
+alert('Сумма нечетных чисел: ' + sumOddNumbers(operand));
+
+
+
+
