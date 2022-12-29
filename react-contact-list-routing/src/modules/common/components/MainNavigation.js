a3073d31b6b4feac47c5b9abd023244032aabd76
Seregapiven <seregapiven@gmail.com>
2022-12-29 14:05:47 +0200

    add icon

--- react-contact-list-routing/src/modules/common/components/MainNavigation.js
+++ react-contact-list-routing/src/modules/common/components/MainNavigation.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ContactMailOutlinedIcon from '@mui/icons-material/ContactMailOutlined';
 
 function MainNavigation() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Button variant='h5' sx={{ flexGrow: 1 }} to='/' component={NavLink} >
+        <Button variant='h5' sx={{ flexGrow: 1 }} to='/' component={NavLink} startIcon={<ContactMailOutlinedIcon/>} >
           Contact List
         </Button>
         <Button color='inherit' to='users' component={NavLink} startIcon={<AccountCircleOutlinedIcon/>}>Users</Button>
